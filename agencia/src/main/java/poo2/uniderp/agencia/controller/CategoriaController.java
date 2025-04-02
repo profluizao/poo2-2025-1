d4b67b9ee55f26c3dc0a602ef861dc45ae878ef9
Prof. Luiz Augusto Rodrigues <prof.luiz.a.rodrigues.2018@gmail.com>
2025-04-01 20:18:49 -0400

    Atualização Encontro 07
    Criação dos verbos Post, Put e Delete;

--- agencia/src/main/java/poo2/uniderp/agencia/controller/CategoriaController.java
+++ agencia/src/main/java/poo2/uniderp/agencia/controller/CategoriaController.java
@@ -3,7 +3,12 @@ package poo2.uniderp.agencia.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +29,25 @@ public class CategoriaController {
     @GetMapping
     public List<Categoria> obterTodos(){
         return this.servico.browse();
+    }
+
+    @GetMapping("/{codigo}")
+    public Categoria obterPorId(@PathVariable Long codigo){
+        return this.servico.read(codigo);
+    }
+
+    @PostMapping
+    public Categoria inserir(@RequestBody Categoria instancia){
+        return this.servico.add(instancia);
+    }
+
+    @PutMapping
+    public Categoria alterar(@RequestBody Categoria instancia){
+        return this.servico.edit(instancia);
+    }
+
+    @DeleteMapping("/{codigo}")
+    public Categoria apagar(@PathVariable Long codigo){
+        return this.servico.delete(codigo);
     }
 }
